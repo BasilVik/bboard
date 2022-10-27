a0f46269f31d5095f3df620f782770c8c0a0c73c
Vasiliy <vikkhlyaev.vasiliy@yandex.ru>
2022-10-27 12:00:04 +0700

    create AdvUser model

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,3 +1,19 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(
+        default=True,
+        db_index=True,
+        verbose_name='Прошел активацию?'
+    )
+
+    send_messages = models.BigAutoField(
+        default=True,
+        verbose_name='Присылать оповещения о новых комментариях?'
+    )
+
+    class Meta(AbstractUser.Meta):
+        pass
