bd41ae2069a6768bc34d8e1506be33499aab6845
Vasiliy <vikkhlyaev.vasiliy@yandex.ru>
2022-10-28 15:25:24 +0700

    add profile

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,11 +1,12 @@
 import imp
 from django.urls import path
 
-from .views import index, other_page, BBLoginView
+from .views import index, other_page, profile, BBLoginView
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
