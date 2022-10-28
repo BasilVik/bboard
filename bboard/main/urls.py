db3384c7bb56b410b2e38aa7c9aa6188a6e5ce9b
Vasiliy <vikkhlyaev.vasiliy@yandex.ru>
2022-10-28 17:23:52 +0700

    add profile_change

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,13 +1,41 @@
 import imp
 from django.urls import path
 
-from .views import index, other_page, profile, BBLoginView, BBLogoutView
+from .views import (
+    index,
+    other_page,
+    profile,
+    BBLoginView,
+    BBLogoutView,
+    ChangeUserInfoView
+)
 
 app_name = 'main'
 urlpatterns = [
-    path('accounts/login/', BBLoginView.as_view(), name='login'),
-    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
-    path('accounts/profile/', profile, name='profile'),
-    path('<str:page>/', other_page, name='other'),
+    path(
+        'accounts/login/',
+        BBLoginView.as_view(),
+        name='login'
+    ),
+    path(
+        'accounts/logout/',
+        BBLogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        'accounts/profile/change/',
+        ChangeUserInfoView.as_view(),
+        name='profile_change'
+    ),
+    path(
+        'accounts/profile/',
+        profile,
+        name='profile'
+    ),
+    path(
+        '<str:page>/',
+        other_page,
+        name='other'
+    ),
     path('', index, name='index'),
 ]
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -5,6 +5,13 @@ from django.template.loader import get_template
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
 
 
 def index(request):
@@ -19,6 +26,11 @@ def other_page(request, page):
     return HttpResponse(template.render(request=request))
 
 
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
 
@@ -27,6 +39,18 @@ class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
 
 
-@login_required
-def profile(request):
-    return render(request, 'main/profile.html')
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
