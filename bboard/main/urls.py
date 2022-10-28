937e59616bff2f37069526a6278cfa780bb327c4
Vasiliy <vikkhlyaev.vasiliy@yandex.ru>
2022-10-28 15:35:50 +0700

    add logout

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,11 +1,12 @@
 import imp
 from django.urls import path
 
-from .views import index, other_page, profile, BBLoginView
+from .views import index, other_page, profile, BBLoginView, BBLogoutView
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
