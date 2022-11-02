a0c29c340f69cf805fe1fcbfca11ac618138e7c4
Vasiliy <vikkhlyaev.vasiliy@yandex.ru>
2022-11-02 12:20:28 +0700

    add rubrics

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,4 +1,7 @@
 from email.policy import default
+from enum import unique
+from tabnanny import verbose
+from weakref import proxy
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -17,3 +20,65 @@ class AdvUser(AbstractUser):
 
     class Meta(AbstractUser.Meta):
         pass
+
+
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        unique=True,
+        verbose_name='Название'
+    )
+    order = models.SmallIntegerField(
+        default=0,
+        db_index=True,
+        verbose_name='Порядок'
+    )
+    super_rubric = models.ForeignKey(
+        'SuperRubric',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        verbose_name='Надрубрика'
+    )
+
+
+class SuperRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric__isnull=True)
+
+
+class SuperRubric(Rubric):
+    objects = SuperRubricManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        proxy = True
+        ordering = ('order', 'name')
+        verbose_name = 'Надрубрика'
+        verbose_name_plural = 'Надрубрики'
+
+
+class SubRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric__isnull=False)
+
+
+class SubRubric(Rubric):
+    objects = SubRubricManager()
+
+    def __str__(self):
+        f'{self.super_rubric.__str__} - {self.name}'
+
+    class Meta:
+        proxy = True
+        ordering = (
+            'super_rubric__order',
+            'super_rubric__name',
+            'order',
+            'name'
+        )
+        verbose_name = 'Подрубрика'
+        verbose_name_plural = 'Подрубрики'
